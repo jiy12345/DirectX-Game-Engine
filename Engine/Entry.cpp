2f499364a2e56796514c6a10ae30f9a70f58f8d6
jiy12345 <jiy12367@gmail.com>
2022-01-20 03:45:35 +0900

    feat: create the winMain function.

---
+++ Engine/Entry.cpp
@@ -0,0 +1,11 @@
+#include<Windows.h>
+
+
+int APIENTRY WinMain(
+	_In_ HINSTANCE const hInstance,
+	_In_opt_ HINSTANCE const prevhInstance,
+	_In_ LPSTR const lpCmdLine,
+	_In_ int const nShowCmd
+) {
+
+}
