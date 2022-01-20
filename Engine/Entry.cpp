3fc795c5908e115c8cca7dea7576e5e4ffc9f277
jiy12345 <jiy12367@gmail.com>
2022-01-21 03:25:52 +0900

    feat: Create and register a window class

--- Engine/Entry.cpp
+++ Engine/Entry.cpp
@@ -7,5 +7,20 @@ int APIENTRY WinMain(
 	_In_ LPSTR const lpCmdLine,
 	_In_ int const nShowCmd
 ) {
+	HWND hWindow = nullptr;
 
+	{
+		WNDCLASSEX wndClass = WNDCLASSEX();
+
+		wndClass.cbSize = sizeof(WNDCLASSEX);
+		wndClass.lpfnWndProc = nullptr;
+		wndClass.hInstance = hInstance;
+		wndClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+		wndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+		wndClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+		wndClass.lpszClassName = "Window";
+		wndClass.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+
+		RegisterClassEx(&wndClass);
+	}
 }
